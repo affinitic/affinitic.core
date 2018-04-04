9a368c6529790fc562f2277ddd9cf05709bd6d5c
AuroreMariscal <aurore@affinitic.be>
2018-04-04 10:54:34 +0200

    Adapt homepage
    This refs affinitic ##

--- affinitic/core/browser/homepageview.py
+++ affinitic/core/browser/homepageview.py
@@ -1,7 +1,8 @@
-
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from Products.Five import BrowserView
+from urllib2 import urlopen
+import json
 
 
 class HomepageView(BrowserView):
@@ -16,3 +17,21 @@ class HomepageView(BrowserView):
         catalog = getattr(self.context, 'portal_catalog')
         brains = catalog.searchResults(Subject=(tag_name))
         return brains
+
+    def getNumberRepoGit(self):
+        try:
+            response = urlopen("https://api.github.com/users/affinitic")
+            data = response.read().decode("utf-8")
+            json_data = json.loads(data)
+            repo = json_data["public_repos"]
+        except:
+            repo = None
+        return repo
+
+    def getBirthday(self):
+        date_now = datetime.now()
+        year_now = date_now.year
+        date_birth = datetime(2006, 7, 11)
+        year_birth = date_birth.year
+        birthday = year_now - year_birth
+        return birthday
